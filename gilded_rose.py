4b68eb26f8b4ec3129f7f5b4fae7aec45469347e
Devan Rehunathan <rehunathan@gmail.com>
2022-07-18 12:35:40 +0800

    Removing duplicate code for 'Aged Brie'

--- gilded_rose.py
+++ gilded_rose.py
@@ -10,7 +10,7 @@ class GildedRose(object):
             if item.name == "Aged Brie":
                 if item.quality < 50:
                     item.quality = item.quality + 1
-            elif item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert":
+            elif item.name != "Backstage passes to a TAFKAL80ETC concert":
                 if item.quality > 0:
                     if item.name != "Sulfuras, Hand of Ragnaros":
                         item.quality = item.quality - 1
@@ -27,16 +27,12 @@ class GildedRose(object):
             if item.name != "Sulfuras, Hand of Ragnaros":
                 item.sell_in = item.sell_in - 1
             if item.sell_in < 0:
-                if item.name != "Aged Brie":
-                    if item.name != "Backstage passes to a TAFKAL80ETC concert":
-                        if item.quality > 0:
-                            if item.name != "Sulfuras, Hand of Ragnaros":
-                                item.quality = item.quality - 1
-                    else:
-                        item.quality = item.quality - item.quality
-                else:
-                    if item.quality < 50:
-                        item.quality = item.quality + 1
+               if item.name != "Backstage passes to a TAFKAL80ETC concert":
+                   if item.quality > 0:
+                       if item.name != "Sulfuras, Hand of Ragnaros":
+                           item.quality = item.quality - 1
+               else:
+                   item.quality = item.quality - item.quality
 
 
 class Item:
