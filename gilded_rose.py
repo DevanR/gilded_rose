53c67d7363eab8bb1e829f3472430e6a5f12fa5e
Devan Rehunathan <rehunathan@gmail.com>
2022-07-18 13:13:35 +0800

    Extracted logic for 'Sulfuras'

--- gilded_rose.py
+++ gilded_rose.py
@@ -10,10 +10,11 @@ class GildedRose(object):
             if item.name == "Aged Brie":
                 if item.quality < 50:
                     item.quality = item.quality + 1
+            elif item.name == "Sulfuras, Hand of Ragnaros":
+                item.sell_in = item.sell_in + 1
             elif item.name != "Backstage passes to a TAFKAL80ETC concert":
                 if item.quality > 0:
-                    if item.name != "Sulfuras, Hand of Ragnaros":
-                        item.quality = item.quality - 1
+                    item.quality = item.quality - 1
             else:
                 if item.quality < 50:
                     item.quality = item.quality + 1
@@ -24,13 +25,13 @@ class GildedRose(object):
                         if item.sell_in < 6:
                             if item.quality < 50:
                                 item.quality = item.quality + 1
-            if item.name != "Sulfuras, Hand of Ragnaros":
-                item.sell_in = item.sell_in - 1
+
+            item.sell_in = item.sell_in - 1
+
             if item.sell_in < 0:
                if item.name != "Backstage passes to a TAFKAL80ETC concert":
                    if item.quality > 0:
-                       if item.name != "Sulfuras, Hand of Ragnaros":
-                           item.quality = item.quality - 1
+                        item.quality = item.quality - 1
                else:
                    item.quality = item.quality - item.quality
 
