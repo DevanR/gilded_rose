170c4c3bcbe16b2e95922571d2c6e366c7abd1c4
Devan Rehunathan <rehunathan@gmail.com>
2022-07-18 14:46:15 +0800

    Gave each branch its own function.

--- gilded_rose.py
+++ gilded_rose.py
@@ -6,35 +6,66 @@ class GildedRose(object):
         self.items = items
 
     def update_quality(self):
-        for item in self.items:
-            if item.name == "Aged Brie":
-                if item.quality < 50:
-                    item.quality = item.quality + 1
-            elif item.name == "Sulfuras, Hand of Ragnaros":
-                item.sell_in = item.sell_in + 1
-            elif item.name == "Backstage passes to a TAFKAL80ETC concert":
+
+        def aged_brie(item):
+            if item.quality < 50:
                 item.quality = item.quality + 1
-                if item.sell_in < 11:
-                    if item.quality < 50:
-                        item.quality = item.quality + 1
-                if item.sell_in < 6:
-                    if item.quality < 50:
-                        item.quality = item.quality + 1
-                if item.sell_in <= 0:
-                    item.quality = 0
-            elif item.name == "Conjured Mana Cake":
-                if item.quality > 2:
-                    item.quality = item.quality - 2
-                elif item.quality == 1:
-                    item.quality = item.quality - 1
-            else:
-                if item.quality > 0:
-                    item.quality = item.quality - 1
 
             item.sell_in = item.sell_in - 1
             if item.sell_in < 0 and item.quality > 0:
                 item.quality = item.quality - 1
 
+        def sulfuras(item):
+            item.sell_in = item.sell_in + 1
+            item.sell_in = item.sell_in - 1
+
+            if item.sell_in < 0 and item.quality > 0:
+                item.quality = item.quality - 1
+
+        def backstage_pass(item):
+            item.quality = item.quality + 1
+            if item.sell_in < 11:
+                if item.quality < 50:
+                    item.quality = item.quality + 1
+            if item.sell_in < 6:
+                if item.quality < 50:
+                    item.quality = item.quality + 1
+            if item.sell_in <= 0:
+                item.quality = 0
+
+            item.sell_in = item.sell_in - 1
+            if item.sell_in < 0 and item.quality > 0:
+                item.quality = item.quality - 1
+
+        def conjured(item):
+            if item.quality > 2:
+                item.quality = item.quality - 2
+            elif item.quality == 1:
+                item.quality = item.quality - 1
+
+            item.sell_in = item.sell_in - 1
+            if item.sell_in < 0 and item.quality > 0:
+                item.quality = item.quality - 1
+
+        def regular(item):
+            if item.quality > 0:
+                item.quality = item.quality - 1
+
+            item.sell_in = item.sell_in - 1
+            if item.sell_in < 0 and item.quality > 0:
+                item.quality = item.quality - 1
+
+        for item in self.items:
+            if item.name == "Aged Brie":
+                aged_brie(item)
+            elif item.name == "Sulfuras, Hand of Ragnaros":
+                sulfuras(item)
+            elif item.name == "Backstage passes to a TAFKAL80ETC concert":
+                backstage_pass(item)
+            elif item.name == "Conjured Mana Cake":
+                conjured(item)
+            else:
+                regular(item)
 
 class Item:
     def __init__(self, name, sell_in, quality):
@@ -44,3 +75,4 @@ class Item:
 
     def __repr__(self):
         return "%s, %s, %s" % (self.name, self.sell_in, self.quality)
+
