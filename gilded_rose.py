2c2458715fb57d62e5826cc7c5feb755993eeef7
Devan Rehunathan <rehunathan@gmail.com>
2022-07-18 14:54:03 +0800

    Extracted functions. Thinking about future.

--- gilded_rose.py
+++ gilded_rose.py
@@ -1,59 +1,64 @@
 # -*- coding: utf-8 -*-
 
+def aged_brie(item):
+    if item.quality < 50:
+        item.quality = item.quality + 1
+
+    item.sell_in = item.sell_in - 1
+    if item.sell_in < 0 and item.quality > 0:
+        item.quality = item.quality - 1
+
+
+def sulfuras(item):
+    item.sell_in = item.sell_in + 1
+    item.sell_in = item.sell_in - 1
+
+    if item.sell_in < 0 and item.quality > 0:
+        item.quality = item.quality - 1
+
+
+def backstage_pass(item):
+    item.quality = item.quality + 1
+    if item.sell_in < 11:
+        if item.quality < 50:
+            item.quality = item.quality + 1
+    if item.sell_in < 6:
+        if item.quality < 50:
+            item.quality = item.quality + 1
+    if item.sell_in <= 0:
+        item.quality = 0
+
+    item.sell_in = item.sell_in - 1
+    if item.sell_in < 0 and item.quality > 0:
+        item.quality = item.quality - 1
+
+
+def conjured(item):
+    if item.quality > 2:
+        item.quality = item.quality - 2
+    elif item.quality == 1:
+        item.quality = item.quality - 1
+
+    item.sell_in = item.sell_in - 1
+    if item.sell_in < 0 and item.quality > 0:
+        item.quality = item.quality - 1
+
+
+def regular(item):
+    if item.quality > 0:
+        item.quality = item.quality - 1
+
+    item.sell_in = item.sell_in - 1
+    if item.sell_in < 0 and item.quality > 0:
+        item.quality = item.quality - 1
+
+
 class GildedRose(object):
 
     def __init__(self, items):
         self.items = items
 
     def update_quality(self):
-
-        def aged_brie(item):
-            if item.quality < 50:
-                item.quality = item.quality + 1
-
-            item.sell_in = item.sell_in - 1
-            if item.sell_in < 0 and item.quality > 0:
-                item.quality = item.quality - 1
-
-        def sulfuras(item):
-            item.sell_in = item.sell_in + 1
-            item.sell_in = item.sell_in - 1
-
-            if item.sell_in < 0 and item.quality > 0:
-                item.quality = item.quality - 1
-
-        def backstage_pass(item):
-            item.quality = item.quality + 1
-            if item.sell_in < 11:
-                if item.quality < 50:
-                    item.quality = item.quality + 1
-            if item.sell_in < 6:
-                if item.quality < 50:
-                    item.quality = item.quality + 1
-            if item.sell_in <= 0:
-                item.quality = 0
-
-            item.sell_in = item.sell_in - 1
-            if item.sell_in < 0 and item.quality > 0:
-                item.quality = item.quality - 1
-
-        def conjured(item):
-            if item.quality > 2:
-                item.quality = item.quality - 2
-            elif item.quality == 1:
-                item.quality = item.quality - 1
-
-            item.sell_in = item.sell_in - 1
-            if item.sell_in < 0 and item.quality > 0:
-                item.quality = item.quality - 1
-
-        def regular(item):
-            if item.quality > 0:
-                item.quality = item.quality - 1
-
-            item.sell_in = item.sell_in - 1
-            if item.sell_in < 0 and item.quality > 0:
-                item.quality = item.quality - 1
 
         for item in self.items:
             if item.name == "Aged Brie":
