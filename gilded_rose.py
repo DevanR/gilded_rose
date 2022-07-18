8ebc258c771c0b6a11485ae0eeef8177ab61b6ee
Devan Rehunathan <rehunathan@gmail.com>
2022-07-18 12:31:15 +0800

    Added duplicate code to extract logic for 'Aged Brie'

--- gilded_rose.py
+++ gilded_rose.py
@@ -7,7 +7,10 @@ class GildedRose(object):
 
     def update_quality(self):
         for item in self.items:
-            if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert":
+            if item.name == "Aged Brie":
+                if item.quality < 50:
+                    item.quality = item.quality + 1
+            elif item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert":
                 if item.quality > 0:
                     if item.name != "Sulfuras, Hand of Ragnaros":
                         item.quality = item.quality - 1
